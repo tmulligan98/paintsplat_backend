1a60e657261a68de62a51acf9a8affa480ace929
Conor <conorcdalton99@gmail.com>
2021-10-18 18:03:00 +0100

    Added movement logic to canvas.js

--- constants.js
+++ constants.js
@@ -4,5 +4,12 @@
 module.exports = Object.freeze({
 
     PLAYER_FIRE_COOLDOWN: 2.0,
-    SPLAT_RADIUS = 5.0
+    SPLAT_RADIUS: 5.0,
+    SCREEN_HEIGHT: 200,
+    SCREEN_WIDTH: 200,
+    CANVAS_HEIGHT: 80,
+    CANVAS_WIDTH: 80,
+    MAX_ACCELERATION: 3,
+    MAX_SPEED: 20
+
 });
